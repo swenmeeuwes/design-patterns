c9e7a8e4aecc9f61ecaa18f092119a4c4e72a01b
Swen <swenmeeu@gmail.com>
2018-02-15 14:31:00 +0100

    Refactored duplicated lines to for-loops

--- Command/UndoFunctionality/UndoFunctionality/Program.cs
+++ Command/UndoFunctionality/UndoFunctionality/Program.cs
@@ -7,14 +7,24 @@ namespace UndoFunctionality
         static void Main(string[] args)
         {
             var commandExecuter = new CommandExecuter();
-            commandExecuter.ExecuteCommand(new PasteCommand());
-            commandExecuter.ExecuteCommand(new PasteCommand());
-            commandExecuter.ExecuteCommand(new PasteCommand());
 
-            commandExecuter.CommandHistory.Undo();
+            // Paste the mock clipboard text
+            for (var i = 0; i < 4; i++)
+            {
+                commandExecuter.ExecuteCommand(new PasteCommand());
+            }
 
-            commandExecuter.CommandHistory.Redo();
-            commandExecuter.CommandHistory.Redo();
+            // Do too many undo's to check for exceptions
+            for (var i = 0; i < 8; i++)
+            {
+                commandExecuter.CommandHistory.Undo();
+            }
+
+            // Test redo
+            for (var i = 0; i < 2; i++)
+            {
+                commandExecuter.CommandHistory.Redo();
+            }
 
             Console.WriteLine(MockTextField.Text);
 
